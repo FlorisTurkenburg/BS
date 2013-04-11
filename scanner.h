3baa2cd5f9ce8ae339256b71ac42c3240f031006
Floris Turkenburg <sk8_floris@hotmail.com>
2013-04-11 13:35:44 +0200

    prototypes in headerfile

--- scanner.h
+++ scanner.h
@@ -10,7 +10,22 @@
 #include <errno.h>
 #include <sys/wait.h>
 
+typedef int builtinFun (char *command);
+
+struct builtin_Func {
+    builtinFun *fun;
+    char name[32];
+};
+
 void run_shell();
 int check_allocation(void *);
+int do_exit(char *command);
+int do_cd(char *command);
+int do_source(char *command);
+void free_array(char ***array);
+char *trimwhitespace(char *str);
+void executeCommand (unsigned char *commandStr);
+void parseCommand (unsigned char *commandStr);
+int scanLine(FILE *fd);
 
 #endif
