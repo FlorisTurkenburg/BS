5feb954c25bdb55aadbc3f66358892f5dcb51a9d
Floris Turkenburg <sk8_floris@hotmail.com>
2013-04-11 15:58:12 +0200

    kan make_pipes niet vinden?

--- scanner.c
+++ scanner.c
@@ -145,7 +145,7 @@ void parseCommand(unsigned char *command_string) {
     unsigned char **segments;
 
     pipe_count = split_string(command_string, &segments, '|') - 1;
-    pipes = create_pipes(pipe_count);
+    pipes = make_pipes(pipe_count);
 
     for(int i = 0; segments[i]; i++) {
         pid = executeCommand(trimwhitespace(segments[i]), pipes);
